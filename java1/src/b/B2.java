08fcaf9e3a1cfdd26c39cd69061fc2d73d335bab
hashep <gooboo712@gmail.com>
2024-04-15 15:48:04 +0900

    stash

---
+++ java1/src/b/B2.java
@@ -0,0 +1,16 @@
+package b;
+
+public class B2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// System.in.read();
+		
+		System.out.println("say: what");
+		System.out.printf("say: %s", "what\n");
+		System.out.printf("say: %d", 56);
+		System.out.println();
+		System.out.printf("%1$d jajaja %2$s", 56, "ebebee");
+	}
+
+}
---
+++ java1/src/b/B3.java
@@ -0,0 +1,12 @@
+package b;
+
+public class B3 {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		int value = System.in.read(); 
+		
+		System.out.print("type = %d" + value);
+	}
+
+}
