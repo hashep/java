b4783d623e1336635bc00a9ea7fd693d4e24bc1b
hashep <gooboo712@gmail.com>
2024-05-02 18:02:58 +0900

    sm3l

---
+++ java1/src/c/C1.java
@@ -0,0 +1,11 @@
+package c;
+
+public class C1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/* 연산자 사용 => long이 하나라도 포함되면 long으로, double이 하나라도포함되면 double,
+		타입에 byte short char밖에 없으면 전부 int로 변환 */
+	}
+
+}
