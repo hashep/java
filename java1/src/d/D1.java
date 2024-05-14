538ae41f333b88ece865de707f52abac52dfeef3
hashep <gooboo712@gmail.com>
2024-05-14 19:02:51 +0900

    3

---
+++ java1/src/d/D1.java
@@ -0,0 +1,10 @@
+package d;
+
+public class D1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		D1_1 A = new D1_1();
+	}
+
+}
---
+++ java1/src/d/D1_1.java
@@ -0,0 +1,9 @@
+package d;
+
+public class D1_1 {
+
+}
+
+class Shit{
+	
+}
