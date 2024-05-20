62c2bb7902ea850dce8e32681d93ddc28c2fb01f
hashep <gooboo712@gmail.com>
2024-05-20 23:09:17 +0900

    asda

---
+++ java1/src/d/D3.java
@@ -0,0 +1,13 @@
+package d;
+
+public class D3 {
+	String nation = "대한민국";
+	String name;
+	String ssn;
+	
+	public D3(String n,String s) {
+		name = n;
+		ssn = s;
+	}
+
+}
---
+++ java1/src/d/D3_1.java
@@ -0,0 +1,8 @@
+package d;
+
+public class D3_1 {
+	public static void main(String[] args) {
+		D3 a = new D3("shit", "2312321312");
+		System.out.println(a.name + a.ssn + a.nation);
+	}
+}
